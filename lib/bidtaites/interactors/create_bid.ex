b61c889c96ef59990b07710f95be248c406001b2
Alvaro Ferreira <alvaro.ferreira@talkdesk.com>
2019-03-22 16:52:11 +0000

    change value in creation bid

--- lib/bidtaites/interactors/create_bid.ex
+++ lib/bidtaites/interactors/create_bid.ex
@@ -9,7 +9,7 @@ defmodule Bidtaites.Interactors.CreateBid do
 
     case Bids.last(auction_id) do
       nil -> bid_correct(bid)
-      max_bid when max_bid.value < val -> bid_correct(bid)
+      %{"value" => value} when value < val -> bid_correct(bid)
       _ -> %{error: "error creating bid: bid lower than value."}
     end
 
